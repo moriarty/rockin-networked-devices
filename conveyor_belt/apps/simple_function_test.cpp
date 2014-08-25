f78dda8c0c6f0a95d29c48fa92c460382225858c
Frederik Hegger <frederik.hegger@h-brs.de>
2014-08-25 14:58:22 +0200

    add a simple test application

---
+++ conveyor_belt/apps/simple_function_test.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <conveyor_belt_kf_d44.h>
+
+void sleep_with_progress(unsigned int seconds)
+{
+    for(size_t i = 0; i < seconds; ++i)
+    {
+        std::cout << ". " << std::flush;
+        sleep(1);
+    }
+    std::cout << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    // name of the system device
+    const std::string device_name = "/dev/conveyor";
+
+    ConveyorBeltKfD44 *belt = new ConveyorBeltKfD44();
+
+    std::cout << "Connecting to \"" << device_name << "\" ..." << std::endl;
+    if (belt->connect(device_name) != 0)
+    {
+        std::cerr << "... could not connect to the device! " << std::endl;
+        return -1;
+    }
+
+    std::cout << "Set maximum belt velocity (75Hz)" << std::endl;
+    belt->setVelocity(boost::units::quantity<boost::units::si::velocity>(7500 * boost::units::si::meter_per_second));
+
+    std::cout << "Moving the belt in FORWARD direction for 5 seconds " << std::flush;
+    belt->start(FORWARD);
+    sleep_with_progress(5);
+
+    std::cout << "Moving the belt in REVERSE direction for 5 seconds " << std::flush;
+    belt->start(REVERSE);
+    sleep_with_progress(5);
+
+    std::cout << "Stopping the belt" << std::endl;
+    belt->stop();
+
+    delete belt;
+
+    return 0;
+}

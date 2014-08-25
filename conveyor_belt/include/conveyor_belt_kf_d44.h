4dee4500fb38f5d19ddfb619c80696cdc44ecfce
Frederik Hegger <frederik.hegger@h-brs.de>
2014-08-25 14:57:46 +0200

    add class for the conveyor belt KF-D44

---
+++ conveyor_belt/include/conveyor_belt_kf_d44.h
@@ -0,0 +1,125 @@
+/**
+ * \class ConveyorBeltKfD44
+ *
+ * \brief Interface for the conveyor belt KF-D44 with Yaskawa V1000 frequency converter
+ *
+ * \author Frederik Hegger
+ *
+ */
+
+#ifndef CONVEYOR_BELT_KF_D44_H_
+#define CONVEYOR_BELT_KF_D44_H_
+
+#include <bitset>
+#include <boost/units/io.hpp>
+#include <boost/units/systems/si/velocity.hpp>
+#include <modbus/modbus.h>
+
+#define WAIT_TIME_WRITE_PARAMETERS_IN_MS        200     // in milliseconds; Parameter H5-11 is set to "1" (see p. 408 of Yaskawa V100 documentation)
+#define WAIT_TIME_CONTROL_COMMAND_IN_MS         5       // in milliseconds
+#define WAIT_TIME_READ_PARAMETERS_IN_MS         5       // in milliseconds
+
+using namespace boost::units;
+
+enum Direction
+{
+    FORWARD = 0, REVERSE = 1
+};
+enum RunMode
+{
+    STOP = 0, START = 1
+};
+
+class ConveyorBeltKfD44
+{
+    public:
+
+        ConveyorBeltKfD44();
+        virtual ~ConveyorBeltKfD44();
+
+        /**
+         *	Establishes the connection to a conveyor belt device.
+         *
+         * @param device_name the name of the serial interface device as it appears in the system (e.g. "/dev/ttyUSB0")
+         * @retval 0 connection has been successfully established
+         * @retval -1 could not create new RTU context
+         * @retval -2 could not set slave ID
+         * @retval -3 could not establish connection to the specified slave
+         * @retval -4 "Drive Ready" bit could not be read
+         */
+        int connect(const std::string device_name);
+
+        /**
+         * Closes the current connection to the device.
+         */
+        void disconnect();
+
+        /**
+         * Get information whether a connection is to the device is established or not.
+         *
+         * @retval true a connection is established
+         * @retval false no connection
+         */
+        bool is_connected();
+
+        /**
+         * Start the belt movement with a given direction
+         *
+         * @param direction the direction in which the belt should start to move. Either FORWARD or REVERSE
+         * @retval 0 the start command has been written to the respective Modbus register
+         * @retval -1 unsupported direction specified. Only FORWARD and REVERSE are supported.
+         * @retval -2 the start command could not be written to the respective Modbus register
+         */
+        int start(Direction direction);
+
+        /**
+         * Stop the belt movement.
+         *
+         * @retval true the stop command has been written to the respective Modbus register
+         * @retval false the stop command could not be written to the respective Modbus register
+         */
+        bool stop();
+
+        /**
+         * Set the velocity of the belt. The velocity can be changed in any state.
+         *
+         * @param desired_belt_velocity	desired belt velocity in meter per seconds (Minimum velocity = 0.01 m/sec, maximum velocity = 0.03 m/sec). ToDo
+         */
+        void setVelocity(const quantity<si::velocity> desired_belt_velocity);
+
+        /**
+         * Allows to enable and disable the Modbus debug messages.
+         *
+         * @param debug_mode_on if set to TRUE, additional debug messages from the Modbus library will be printed.
+         */
+        void setModbusDebugMode(const bool debug_mode_on);
+
+    private:
+
+        /**
+         * Sets default values for various parameters
+         */
+        void setDefaultParameters();
+
+        /**
+         * Converts a velocity into a frequency
+         *
+         * @param velocity the velocity of the belt given in meters per second
+         * @return a frequency in Hz * 100 (e.g. 7000 = 70Hz)
+         */
+        unsigned int convertVelocityToFrequency(const quantity<si::velocity> velocity);
+
+
+
+        modbus_t *modbus_rtu_contex;
+
+        //ToDO: read these parameter from a config file
+        int slave_id;
+        int baudrate;
+        int data_bits;
+        int stop_bits;
+        char partiy;
+        quantity<si::velocity> default_belt_velocity;
+};
+
+#endif /* CONVEYOR_BELT_KF_D44_H_ */
---
+++ conveyor_belt/src/conveyor_belt_kf_d44.cpp
@@ -0,0 +1,156 @@
+/*
+ * conveyor_belt_kf_d44.cpp
+ *
+ *  Created on: Aug 22, 2014
+ *      Author: Frederik Hegger
+ */
+
+#include "conveyor_belt_kf_d44.h"
+
+ConveyorBeltKfD44::ConveyorBeltKfD44()
+{
+    modbus_rtu_contex = NULL;
+
+    //ToDO: read these parameters from a config file
+    slave_id = 0x1F;
+    baudrate = 9600;
+    data_bits = 8;
+    stop_bits = 1;
+    partiy = 'N';                   // no parity
+
+    default_belt_velocity = 7500 * si::meter_per_second;
+}
+
+ConveyorBeltKfD44::~ConveyorBeltKfD44()
+{
+    disconnect();
+}
+
+int ConveyorBeltKfD44::connect(const std::string device_name)
+{
+    // disconnect an old connection (if one exists) before
+    disconnect();
+
+    // configure the serial connection
+    modbus_rtu_contex = modbus_new_rtu(device_name.c_str(), baudrate, partiy, data_bits, stop_bits);
+
+    if (modbus_rtu_contex == NULL)
+        return -1;
+
+    // set slave id to connect to
+    if (modbus_set_slave(modbus_rtu_contex, slave_id) != 0)
+        return -2;
+
+    // connect to slave device
+    if (modbus_connect(modbus_rtu_contex) != 0)
+        return -3;
+
+    if (!is_connected())
+        return -4;
+
+    setDefaultParameters();
+
+    return 0;
+}
+
+void ConveyorBeltKfD44::disconnect()
+{
+    if (is_connected())
+    {
+        stop();
+        modbus_close(modbus_rtu_contex);
+    }
+
+    if (modbus_rtu_contex != NULL)
+    {
+        modbus_free(modbus_rtu_contex);
+        modbus_rtu_contex = NULL;
+    }
+}
+
+bool ConveyorBeltKfD44::is_connected()
+{
+    if (modbus_rtu_contex == NULL)
+        return false;
+
+    // check if status register can be read
+    uint16_t current_register_value[1];
+    if (modbus_read_registers(modbus_rtu_contex, 0x0001, 1, current_register_value) != 1)
+    {
+        usleep((WAIT_TIME_READ_PARAMETERS_IN_MS * 1000));
+        return false;
+    }
+    usleep((WAIT_TIME_READ_PARAMETERS_IN_MS * 1000));
+
+    return true;
+}
+
+int ConveyorBeltKfD44::start(Direction motor_direction)
+{
+    std::bitset<16> register_value(0);
+
+    if (motor_direction != FORWARD && motor_direction != REVERSE)
+        return -1;
+
+    // set the direction bit
+    register_value.set(1, motor_direction);
+
+    // set run mode to start
+    register_value.set(0, true);
+
+    // write the values to the register
+    if (modbus_write_register(modbus_rtu_contex, 0x0001, (int) (register_value.to_ulong())) == 1)
+    {
+        usleep((WAIT_TIME_WRITE_PARAMETERS_IN_MS * 1000));
+        return 0;
+    }
+    usleep((WAIT_TIME_WRITE_PARAMETERS_IN_MS * 1000));
+
+    return -2;
+}
+
+bool ConveyorBeltKfD44::stop()
+{
+    // write the values to the register
+    if (modbus_write_register(modbus_rtu_contex, 0x0001, 0x00))
+    {
+        usleep((WAIT_TIME_WRITE_PARAMETERS_IN_MS * 1000));
+        return true;
+    }
+    usleep((WAIT_TIME_WRITE_PARAMETERS_IN_MS * 1000));
+
+    return false;
+}
+
+void ConveyorBeltKfD44::setVelocity(const quantity<si::velocity> desired_belt_velocity)
+{
+    int velocity_as_frequency = 0;
+
+    velocity_as_frequency = convertVelocityToFrequency(desired_belt_velocity);
+
+    // write the frequency in to the respective register
+    modbus_write_register(modbus_rtu_contex, 0x0002, velocity_as_frequency);
+    usleep((WAIT_TIME_WRITE_PARAMETERS_IN_MS * 1000));
+}
+
+void ConveyorBeltKfD44::setModbusDebugMode(bool debug_mode_on)
+{
+    if (!is_connected())
+        return;
+
+    modbus_set_debug(modbus_rtu_contex, debug_mode_on);
+}
+
+void ConveyorBeltKfD44::setDefaultParameters()
+{
+    stop();
+    setVelocity(default_belt_velocity);
+}
+
+unsigned int ConveyorBeltKfD44::convertVelocityToFrequency(const quantity<si::velocity> velocity)
+{
+    // ToDo: add the conversion from given velocity to the conveyor belts frequency here.
+    // it is only a dummy function right now
+
+    return velocity.value();
+}
